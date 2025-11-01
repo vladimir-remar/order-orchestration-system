a413dcc9f547b161e350396c42d6d4d8fd38e962
vladimir.remar <vladimir.remar@gmail.com>
2025-11-01 23:49:05 +0100

    fix: adapt settings and docker compose

--- web/gateway/settings.py
+++ web/gateway/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+APPS_DIR = BASE_DIR / "apps"
+if str(APPS_DIR) not in sys.path:
+    sys.path.insert(0, str(APPS_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -31,7 +34,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 DOMAIN_APPS = [
-    "apps.orders",
+    "orders",
 ]
 
 INSTALLED_APPS = [
