aaeb731d0890fb7572c021fe3791585a776eb0c9
vladimir.remar <vladimir.remar@gmail.com>
2025-11-02 00:10:38 +0100

    chore(api): nest orders routes under /api/orders/

--- web/gateway/urls.py
+++ web/gateway/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("monitoring.urls")),
-    path("api/", include("orders.urls", namespace="orders")),
+    path("api/orders/", include("orders.urls", namespace="orders")),
 ]
